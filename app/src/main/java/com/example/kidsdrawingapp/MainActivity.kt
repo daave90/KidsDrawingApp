5b7b8fb25d0108f57a784da27b77bda96f331a15
Dave <maciak.dawid.96@gmail.com>
2021-02-26 18:44:45 +0100

    Implement brush size changing dialog

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.kidsdrawingapp
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val dravingView = findViewById<DrawingView>(R.id.drawing_view)
         dravingView.setSizeForBrush(20.toFloat())
+
+        val brushBtn = findViewById<ImageButton>(R.id.ib_brush)
+        brushBtn.setOnClickListener { showBrushSizeDialog() }
+    }
+
+    private fun showBrushSizeDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Choose brush size")
+
+        val smallBtn = brushDialog.findViewById<ImageButton>(R.id.ib_small_brush)
+        val mediumBtn = brushDialog.findViewById<ImageButton>(R.id.ib_medium_brush)
+        val largeBtn = brushDialog.findViewById<ImageButton>(R.id.ib_large_brush)
+
+        smallBtn.setOnClickListener { changeBrushSizeAndCloseDialog(10.0f, brushDialog) }
+        mediumBtn.setOnClickListener { changeBrushSizeAndCloseDialog(20.0f, brushDialog) }
+        largeBtn.setOnClickListener { changeBrushSizeAndCloseDialog(30.0f, brushDialog) }
+        brushDialog.show()
+    }
+
+    private fun changeBrushSizeAndCloseDialog(size: Float, dialog: Dialog) {
+        val drawingView = findViewById<DrawingView>(R.id.drawing_view)
+        drawingView.setSizeForBrush(size)
+        dialog.dismiss()
     }
 }
